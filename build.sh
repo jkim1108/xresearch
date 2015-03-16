51ddddc4676ec84de7bc9664650b8a2344596296
JKim in node 16 <jonghoon@lix.polytechnique.fr>
2015-03-16 20:36:13 +0100

    compiled

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -Wall -O3 -fopenmp -std=c++11 -fopenmp -I/home/kim/xresearch/include /home/kim/xresearch/src/* -o main.o
+g++ -Wall -O3 -fopenmp -std=c++11 -I/home/kim/xresearch/eigen3/ -I/home/kim/xresearch/include /home/kim/xresearch/src/* -o main.o
