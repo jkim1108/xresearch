24468593d48615531aa621e4bd43d1e173cacfaa
JKim in node 16 <jonghoon@lix.polytechnique.fr>
2015-03-17 16:56:28 +0100

    debugged

--- include/parser.h
+++ include/parser.h
@@ -59,7 +59,7 @@ ublas::matrix<double> getKernelMatrix(vtKernel* model, std::vector<T> testset)
     int i, j;
 
     #pragma omp parallel for private(j) schedule(static, 1)
-    for (i=0; i<1; i++)
+    for (i=0; i<n; i++)
     {
         for (j=i; j<n; j++)
         {
